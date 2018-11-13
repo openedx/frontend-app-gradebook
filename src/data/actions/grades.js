2c890e53f81b5d11391649e2739c96e68a577b79
Alex Dusenbery <adusenbery@edx.org>
2018-11-13 11:36:22 -0500

    Get the basic grade update working.

--- src/data/actions/grades.js
+++ src/data/actions/grades.js
@@ -3,6 +3,9 @@ import {
   FINISHED_FETCHING_GRADES,
   ERROR_FETCHING_GRADES,
   GOT_GRADES,
+  GRADE_UPDATE_REQUEST,
+  GRADE_UPDATE_SUCCESS,
+  GRADE_UPDATE_FAILURE,
 } from '../constants/actionTypes/grades';
 import LmsApiService from '../services/LmsApiService';
 
@@ -10,6 +13,16 @@ const startedFetchingGrades = () => ({ type: STARTED_FETCHING_GRADES });
 const finishedFetchingGrades = () => ({ type: FINISHED_FETCHING_GRADES });
 const errorFetchingGrades = () => ({ type: ERROR_FETCHING_GRADES });
 const gotGrades = grades => ({ type: GOT_GRADES, grades });
+
+const gradeUpdateRequest = () => ({ type: GRADE_UPDATE_REQUEST });
+const gradeUpdateSuccess = responseData => ({
+  type: GRADE_UPDATE_SUCCESS,
+  payload: { responseData },
+})
+const gradeUpdateFailure = error => ({
+  type: GRADE_UPDATE_FAILURE,
+  payload: { error },
+});
 
 const fetchGrades = courseId => (
   (dispatch) => {
@@ -41,6 +54,20 @@ const fetchMatchingUserGrades = (courseId, searchText) => (
   }
 );
 
+const updateGrades = (courseId, updateData) => (
+  (dispatch) => {
+    dispatch(gradeUpdateRequest());
+    return LmsApiService.updateGradebookData(courseId, updateData)
+      .then(response => response.data)
+      .then((data) => {
+        dispatch(gradeUpdateSuccess(data))
+      })
+      .catch((error) => {
+        dispatch(gradeUpdateFailure(error));
+      });
+  }
+);
+
 export {
   startedFetchingGrades,
   finishedFetchingGrades,
@@ -48,4 +75,8 @@ export {
   gotGrades,
   fetchGrades,
   fetchMatchingUserGrades,
+  gradeUpdateRequest,
+  gradeUpdateSuccess,
+  gradeUpdateFailure,
+  updateGrades,
 };
--- src/data/services/LmsApiService.js
+++ src/data/services/LmsApiService.js
@@ -11,6 +11,30 @@ class LmsApiService {
     }
     return apiClient.get(gradebookUrl);
   }
+
+  static updateGradebookData(courseId, updateData) {
+    /*
+       updateData is expected to be a list of objects with the keys 'user_id' (an integer),
+       'usage_id' (a string) and 'grade', which is an object with the keys:
+       'earned_all_override', 'possible_all_override', 'earned_graded_override', and 'possible_graded_override',
+       each of which should be an integer.
+       Example:
+       [
+         {
+            "user_id": 9,
+            "usage_id": "block-v1:edX+DemoX+Demo_Course+type@sequential+block@basic_questions",
+            "grade": {
+              "earned_all_override": 11,
+              "possible_all_override": 11,
+              "earned_graded_override": 11,
+              "possible_graded_override": 11
+            }
+          }
+        ]
+    */
+    const gradebookUrl = `${LmsApiService.baseUrl}/api/grades/v1/gradebook/${courseId}/bulk-update`;
+    return apiClient.post(gradebookUrl, updateData);
+  }
 }
 
 export default LmsApiService;
