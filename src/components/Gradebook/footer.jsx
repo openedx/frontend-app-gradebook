6473bafa3d8b4b7cf5efe10729d0846e405f4584
albemarle <45690905+albemarle@users.noreply.github.com>
2018-12-19 15:16:37 -0500

    edx -> edX

--- src/components/Gradebook/footer.jsx
+++ src/components/Gradebook/footer.jsx
@@ -21,7 +21,7 @@ export default function Footer() {
           <Hyperlink destination="https://www.edx.org/" content={renderLogo()} aria-label="edX Home" />
         </div>
         <div className="area-2">
-          <h2>edx</h2>
+          <h2>edX</h2>
           <ul className="list-unstyled p-0 m-0">
             <li><a href="https://www.edx.org/about-us">About</a></li>
             <li><a href="https://www.edx.org/enterprise">edX for Business</a></li>
