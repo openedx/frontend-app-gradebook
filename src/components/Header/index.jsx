c4a4967659a7faa7e25ab71d8432bbc61ea64d2f
Rick Reilly <rreilly@edx.org>
2019-01-02 16:35:41 -0500

    wip

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Hyperlink } from '@edx/paragon';
+import { Hyperlink, Icon } from '@edx/paragon';
+import classNames from 'classnames';
+import Media from 'react-media';
 
 import EdxLogo from '../../../assets/edx-sm.png';
 
@@ -20,18 +22,31 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="mb-3">
-        <header className="border-bottom-blue">
-          <div className="d-flex justify-content-center align-items-center p-3 border-bottom-blue">
-            <Hyperlink content={this.renderLogo()} destination="https://www.edx.org" />
-          </div>
-          <div className>
-            <a className="nav-link border-bottom-gray" href="https://www.google.com">Rick</a>
-            <a className="nav-link border-bottom-gray" href="https://www.google.com">Alex</a>
-            <a className="nav-link border-bottom-gray" href="https://www.google.com">Zach</a>
-            <a className="nav-link border-bottom-gray" href="https://www.google.com">Jansen</a>
-            <a className="nav-link border-bottom-gray" href="https://www.google.com">Simon</a>
-          </div>
-        </header>
+        <Media query="(max-width: 992px)">
+          {matches =>
+            (matches ? (
+              <header className="border-bottom-blue">
+                <div className="d-flex justify-content-center align-items-center p-3 border-bottom-blue">
+                  <button className="border-0 mr-auto" onClick={() => this.setState({ mobileNavOpen: !this.state.mobileNavOpen })}>
+                    <Icon className={classNames('fa', 'fa-2x', 'color-gray', { 'fa-bars': !this.state.mobileNavOpen }, { 'fa-times': this.state.mobileNavOpen })} />
+                  </button>
+                  <Hyperlink content={this.renderLogo()} destination="https://www.edx.org" />
+                </div>
+                {this.state.mobileNavOpen &&
+                  <div>
+                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Rick</a>
+                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Alex</a>
+                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Zach</a>
+                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Jansen</a>
+                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Simon</a>
+                  </div>}
+              </header>
+          ) : (
+            <header>
+              <h1>hello world</h1>
+            </header>))
+        }
+        </Media>
       </div>
     );
   }
