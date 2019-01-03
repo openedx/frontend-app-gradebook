7eedc58e8699410e8f2cd3683c5ddddfa1caf2ec
Rick Reilly <rreilly@edx.org>
2019-01-03 10:26:38 -0500

    wip

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -22,23 +22,23 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="mb-3">
-        <Media query="(max-width: 992px)">
+        <Media query="(max-width: 901px)">
           {matches =>
             (matches ? (
               <header className="border-bottom-blue">
-                <div className="d-flex justify-content-center align-items-center p-3 border-bottom-blue">
-                  <button className="border-0 mr-auto" onClick={() => this.setState({ mobileNavOpen: !this.state.mobileNavOpen })}>
+                <div className="p-3 border-bottom-blue">
+                  <button className="border-0" onClick={() => this.setState({ mobileNavOpen: !this.state.mobileNavOpen })}>
                     <Icon className={classNames('fa', 'fa-2x', 'color-gray', { 'fa-bars': !this.state.mobileNavOpen }, { 'fa-times': this.state.mobileNavOpen })} />
                   </button>
-                  <Hyperlink content={this.renderLogo()} destination="https://www.edx.org" />
+                  <Hyperlink className="header-logo" content={this.renderLogo()} destination="https://www.edx.org" />
                 </div>
                 {this.state.mobileNavOpen &&
                   <div>
-                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Rick</a>
-                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Alex</a>
-                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Zach</a>
-                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Jansen</a>
-                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Simon</a>
+                    <a className="nav-link border-bottom-gray" href="https://support.edx.org">Help</a>
+                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Dashboard</a>
+                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Profile</a>
+                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Account</a>
+                    <a className="nav-link border-bottom-gray" href="https://www.google.com">Sign Out</a>
                   </div>}
               </header>
           ) : (
