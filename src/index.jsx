c235da8cc02f2a7bb2aaa60eceef9447d3b3741f
Adam Stankiewicz <agstanki@gmail.com>
2019-05-16 12:52:51 -0700

    fix forward for including intlprovider

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import SiteFooter from '@edx/frontend-component-footer';
+import { IntlProvider } from 'react-intl';
 
 import {
   faFacebookSquare,
@@ -55,39 +56,41 @@ const socialLinks = [
 ];
 
 const App = () => (
-  <Provider store={store}>
-    <Router>
-      <div>
-        <Header />
-        <main>
-          <Switch>
-            <Route exact path="/:courseId" component={GradebookPage} />
-          </Switch>
-        </main>
-        <SiteFooter
-          siteName={process.env.SITE_NAME}
-          siteLogo={FooterLogo}
-          marketingSiteBaseUrl={process.env.MARKETING_SITE_BASE_URL}
-          supportUrl={process.env.SUPPORT_URL}
-          contactUrl={process.env.CONTACT_URL}
-          openSourceUrl={process.env.OPEN_SOURCE_URL}
-          termsOfServiceUrl={process.env.TERMS_OF_SERVICE_URL}
-          privacyPolicyUrl={process.env.PRIVACY_POLICY_URL}
-          appleAppStoreUrl={process.env.APPLE_APP_STORE_URL}
-          googlePlayUrl={process.env.GOOGLE_PLAY_URL}
-          socialLinks={socialLinks}
-          enterpriseMarketingLink={{
-            url: process.env.ENTERPRISE_MARKETING_URL,
-            queryParams: {
-              utm_source: process.env.ENTERPRISE_MARKETING_UTM_SOURCE,
-              utm_campaign: process.env.ENTERPRISE_MARKETING_UTM_CAMPAIGN,
-              utm_medium: process.env.ENTERPRISE_MARKETING_FOOTER_UTM_MEDIUM,
-            },
-          }}
-        />
-      </div>
-    </Router>
-  </Provider>
+  <IntlProvider>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Header />
+          <main>
+            <Switch>
+              <Route exact path="/:courseId" component={GradebookPage} />
+            </Switch>
+          </main>
+          <SiteFooter
+            siteName={process.env.SITE_NAME}
+            siteLogo={FooterLogo}
+            marketingSiteBaseUrl={process.env.MARKETING_SITE_BASE_URL}
+            supportUrl={process.env.SUPPORT_URL}
+            contactUrl={process.env.CONTACT_URL}
+            openSourceUrl={process.env.OPEN_SOURCE_URL}
+            termsOfServiceUrl={process.env.TERMS_OF_SERVICE_URL}
+            privacyPolicyUrl={process.env.PRIVACY_POLICY_URL}
+            appleAppStoreUrl={process.env.APPLE_APP_STORE_URL}
+            googlePlayUrl={process.env.GOOGLE_PLAY_URL}
+            socialLinks={socialLinks}
+            enterpriseMarketingLink={{
+              url: process.env.ENTERPRISE_MARKETING_URL,
+              queryParams: {
+                utm_source: process.env.ENTERPRISE_MARKETING_UTM_SOURCE,
+                utm_campaign: process.env.ENTERPRISE_MARKETING_UTM_CAMPAIGN,
+                utm_medium: process.env.ENTERPRISE_MARKETING_FOOTER_UTM_MEDIUM,
+              },
+            }}
+          />
+        </div>
+      </Router>
+    </Provider>
+  </IntlProvider>
 );
 
 apiClient.ensurePublicOrAuthenticationAndCookies(
