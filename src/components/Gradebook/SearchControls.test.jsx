0f363e5f9804596efd761177eef2d880d198e86e
Nathan Sprenkle <nsprenkle@edx.org>
2021-04-26 16:40:59 -0400

    MOAAAR TESTS

--- src/components/Gradebook/SearchControls.test.jsx
+++ src/components/Gradebook/SearchControls.test.jsx
@@ -30,6 +30,7 @@ describe('SearchControls', () => {
 
   describe('Component', () => {
     describe('spinner', () => {
+      // These tests aren't strictly necessary since they're already covered by snapshot tests
       it('shows a spinner overlay when the page is waiting for some action to complete', () => {
         const wrapper = searchControls({ showSpinner: true });
         expect(wrapper.find('.spinner-overlay').exists()).toEqual(true);
@@ -41,19 +42,38 @@ describe('SearchControls', () => {
       });
     });
 
-    describe('edit filters button', () => {
-      it('when clicked, sends the signal to toggle the filter drawer', () => {
+    describe('onSubmit', () => {
+      it('calls props.searchForUser with correct data', () => {
         const wrapper = searchControls();
-        const button = wrapper.find('#edit-filters-btn').at(0);
-        button.simulate('click');
+        wrapper.instance().onSubmit('bob');
 
-        expect(props.toggleFilterDrawer).toHaveBeenCalled();
+        expect(props.searchForUser).toHaveBeenCalledWith('course-v1:edX+DEV101+T1', 'bob', 'spring term', 'masters', 'homework');
+      });
+    });
+
+    describe('onChange', () => {
+      it('saves the changed search value to Gradebook state', () => {
+        const wrapper = searchControls();
+        wrapper.instance().onChange('bob');
+        expect(props.setGradebookState).toHaveBeenCalledWith({ filterValue: 'bob' });
+      });
+    });
+
+    describe('onClear', () => {
+      it('re-runs search with existing filters', () => {
+        const wrapper = searchControls();
+        wrapper.instance().onClear('bob');
+        expect(props.getUserGrades).toHaveBeenCalledWith('course-v1:edX+DEV101+T1', 'spring term', 'masters', 'homework');
       });
     });
 
     describe('Snapshots', () => {
-      it('renders', () => {
-        expect(shallow(<SearchControls {...props} />)).toMatchSnapshot();
+      test('basic snapshot', () => {
+        expect(searchControls()).toMatchSnapshot();
+      });
+
+      test('show spinner', () => {
+        expect(searchControls({ showSpinner: true })).toMatchSnapshot();
       });
     });
   });
