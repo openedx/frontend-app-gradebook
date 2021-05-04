ab38e358478e8c73a0516f35307baf424b24a521
Nathan Sprenkle <nsprenkle@edx.org>
2021-05-04 18:14:20 -0400

    test: role selector tests

---
+++ src/data/selectors/roles.test.js
@@ -0,0 +1,23 @@
+import selectors from './roles';
+
+describe('canUserViewGradebook', () => {
+  it('returns true if the user has the canUserViewGradebook role', () => {
+    const canUserViewGradebook = selectors.canUserViewGradebook({
+      roles: {
+        canUserViewGradebook: true,
+        canUserDoTheMonsterMash: false,
+      },
+    });
+    expect(canUserViewGradebook).toBeTruthy();
+  });
+
+  it('returns false if the user does not have the canUserViewGradebook role', () => {
+    const canUserViewGradebook = selectors.canUserViewGradebook({
+      roles: {
+        canUserViewGradebook: false,
+        canUserDoTheMonsterMash: true,
+      },
+    });
+    expect(canUserViewGradebook).toBeFalsy();
+  });
+});
