ec6179e07e5c4744559e4844d5b2ab83315edfbf
Nathan Sprenkle <nsprenkle@edx.org>
2021-05-05 16:14:05 -0400

    test: add more grades selector tests

--- src/data/selectors/grades.test.js
+++ src/data/selectors/grades.test.js
@@ -32,6 +32,89 @@ describe('bulkImportError', () => {
   });
 });
 
+describe('grade formatters', () => {
+  const selectedAssignment = { assignmentId: 'block-v1:edX+type@sequential+block@abcde' };
+
+  describe('formatMinAssignmentGrade', () => {
+    it('passes numbers through grade has been modified and assignment is supplied', () => {
+      const formattedMinAssignmentGrade = selectors.formatMinAssignmentGrade('1', selectedAssignment);
+      const formattedMaxAssignmentGrade = selectors.formatMaxAssignmentGrade('99', selectedAssignment);
+      const formattedMinGrade = selectors.formatMinCourseGrade('37', selectedAssignment);
+      const formattedMaxGrade = selectors.formatMaxCourseGrade('42', selectedAssignment);
+
+      expect(formattedMinAssignmentGrade).toEqual('1');
+      expect(formattedMaxAssignmentGrade).toEqual('99');
+      expect(formattedMinGrade).toEqual('37');
+      expect(formattedMaxGrade).toEqual('42');
+    });
+    it('ignores unmodified grades', () => {
+      const formattedMinAssignmentGrade = selectors.formatMinAssignmentGrade('0', selectedAssignment);
+      const formattedMaxAssignmentGrade = selectors.formatMaxAssignmentGrade('100', selectedAssignment);
+      const formattedMinGrade = selectors.formatMinCourseGrade('0', selectedAssignment);
+      const formattedMaxGrade = selectors.formatMaxCourseGrade('100', selectedAssignment);
+
+      expect(formattedMinAssignmentGrade).toEqual(null);
+      expect(formattedMaxAssignmentGrade).toEqual(null);
+      expect(formattedMinGrade).toEqual(null);
+      expect(formattedMaxGrade).toEqual(null);
+    });
+    it('ignores grade when an assignment is not supplied', () => {
+      const formattedMinAssignmentGrade = selectors.formatMinAssignmentGrade('0', {});
+      const formattedMaxAssignmentGrade = selectors.formatMaxAssignmentGrade('100', {});
+      const formattedMinGrade = selectors.formatMinCourseGrade('0', {});
+      const formattedMaxGrade = selectors.formatMaxCourseGrade('100', {});
+
+      expect(formattedMinAssignmentGrade).toEqual(null);
+      expect(formattedMaxAssignmentGrade).toEqual(null);
+      expect(formattedMinGrade).toEqual(null);
+      expect(formattedMaxGrade).toEqual(null);
+    });
+  });
+});
+
+describe('getExampleSectionBreakdown', () => {
+  const gradesData = {
+    next: null,
+    previous: null,
+    results: [
+      {
+        section_breakdown: [
+          {
+            subsection_name: 'Example Week 1: Getting Started',
+            score_earned: 1,
+            score_possible: 1,
+            percent: 1,
+            displayed_value: '1.00',
+            grade_description: '(0.00/0.00)',
+          },
+        ],
+      },
+    ],
+  };
+
+  it('returns an empty array when results are unavailable', () => {
+    const result = selectors.getExampleSectionBreakdown({ grades: { results: [{}] } });
+    expect(result).toEqual([]);
+  });
+
+  it('returns an empty array when breakdowns are unavailable', () => {
+    const result = selectors.getExampleSectionBreakdown({ grades: { results: [{ foo: 'bar' }] } });
+    expect(result).toEqual([]);
+  });
+
+  it('gets section breakdown when available', () => {
+    const result = selectors.getExampleSectionBreakdown({ grades: gradesData });
+    expect(result).toEqual([{
+      subsection_name: 'Example Week 1: Getting Started',
+      score_earned: 1,
+      score_possible: 1,
+      percent: 1,
+      displayed_value: '1.00',
+      grade_description: '(0.00/0.00)',
+    }]);
+  });
+});
+
 describe('bulkManagementHistoryEntries', () => {
   it('handles history being as-yet unloaded', () => {
     const result = selectors.bulkManagementHistoryEntries({ grades: { bulkManagement: {} } });
