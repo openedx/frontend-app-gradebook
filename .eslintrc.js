bf3c1c19484c4e0864ee4e9f9531c77dea82437b
Ben Warzeski <bwarzeski@edx.org>
2021-06-28 16:35:10 -0400

    ignore accepted import eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 const { createConfig } = require('@edx/frontend-build');
 
-const config = createConfig('eslint');
+const config = createConfig('eslint', {
+  rules: {
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-self-import': 'off',
+    'spaced-comment': ['error', 'always', { 'block': { 'exceptions': ['*'] } }],
+  },
+});
 
 config.settings = {
   "import/resolver": {
