6319f4b85dbafe24bd4e7317bf1bf40e5f591569
Ben Warzeski <bwarzeski@edx.org>
2021-07-01 17:48:50 -0400

    add tests for store aggregator and utils

--- src/data/store.js
+++ src/data/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore } from 'redux';
+import * as redux from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import { createLogger } from 'redux-logger';
@@ -11,26 +11,31 @@ import reducers from './reducers';
 import eventsMap from './services/segment/mapping';
 import { configuration } from '../config';
 
-const loggerMiddleware = createLogger();
+export const createStore = () => {
+  const loggerMiddleware = createLogger();
 
-const middleware = [thunkMiddleware, loggerMiddleware];
-// Conditionally add the segmentMiddleware only if the SEGMENT_KEY environment variable exists.
-if (configuration.SEGMENT_KEY) {
-  middleware.push(createMiddleware(eventsMap, Segment()));
-}
+  const middleware = [thunkMiddleware, loggerMiddleware];
+  // Conditionally add the segmentMiddleware only if the SEGMENT_KEY environment variable exists.
+  if (configuration.SEGMENT_KEY) {
+    middleware.push(createMiddleware(eventsMap, Segment()));
+  }
+  const store = redux.createStore(
+    reducers,
+    composeWithDevTools(redux.applyMiddleware(...middleware)),
+  );
 
-const store = createStore(
-  reducers,
-  composeWithDevTools(applyMiddleware(...middleware)),
-);
+  /**
+   * Dev tools for redux work
+   */
+  if (process.env.NODE_ENV === 'development') {
+    window.store = store;
+    window.actions = actions;
+    window.selectors = selectors;
+  }
 
-/**
- * Dev tools for redux work
- */
-if (process.env.NODE_ENV === 'development') {
-  window.store = store;
-  window.actions = actions;
-  window.selectors = selectors;
-}
+  return store;
+};
+
+const store = createStore();
 
 export default store;
