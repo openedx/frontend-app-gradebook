bee57b8b81569c464c53611c1f604a1c2518593c
Ben Warzeski <bwarzeski@edx.org>
2021-07-30 00:57:36 -0400

    add Import Success toast

---
+++ src/components/GradesView/ImportSuccessToast.messages.js
@@ -0,0 +1,16 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  description: {
+    id: 'gradebook.GradesView.ImportSuccessToast.description',
+    defaultMessage: 'Import Successful! Grades will be updated momentarily.',
+    description: 'Import Success Toast description',
+  },
+  showHistoryViewBtn: {
+    id: 'gradebook.GradesView.ImportSuccessToast.showHistoryViewBtn',
+    defaultMessage: 'View Activity Log',
+    description: 'Button text for action that loads Bulk Management Activity Log view',
+  },
+});
+
+export default messages;
