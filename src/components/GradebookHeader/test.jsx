d33ac3cfa836bf2d1b9d91062d119bfe01d6891f
Ben Warzeski <bwarzeski@edx.org>
2021-07-30 00:55:16 -0400

    add View control button to GradebookHeader if bulk management available

--- src/components/GradebookHeader/test.jsx
+++ src/components/GradebookHeader/test.jsx
@@ -1,42 +1,119 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import { Button } from '@edx/paragon';
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
+
+import actions from 'data/actions';
 import selectors from 'data/selectors';
-import { GradebookHeader, mapStateToProps } from '.';
+import { views } from 'data/constants/app';
+import messages from './messages';
+import { GradebookHeader, mapDispatchToProps, mapStateToProps } from '.';
 
-jest.mock('@edx/frontend-platform/i18n', () => ({
-  defineMessages: messages => messages,
-  FormattedMessage: 'FormattedMessage',
+jest.mock('@edx/paragon', () => ({
+  Button: () => 'Button',
 }));
-
+jest.mock('@edx/frontend-platform/i18n', () => ({
+  defineMessages: m => m,
+  FormattedMessage: () => 'FormattedMessage',
+}));
+jest.mock('data/actions', () => ({
+  __esModule: true,
+  default: {
+    app: { setView: jest.fn() },
+  },
+}));
 jest.mock('data/selectors', () => ({
   __esModule: true,
   default: {
-    app: { courseId: jest.fn(state => ({ courseId: state })) },
+    app: {
+      activeView: jest.fn(state => ({ aciveView: state })),
+      courseId: jest.fn(state => ({ courseId: state })),
+    },
     assignmentTypes: { areGradesFrozen: jest.fn(state => ({ areGradesFrozen: state })) },
     roles: { canUserViewGradebook: jest.fn(state => ({ canUserViewGradebook: state })) },
+    root: { showBulkManagement: jest.fn(state => ({ showBulkManagement: state })) },
   },
 }));
 
 const courseId = 'fakeID';
 describe('GradebookHeader component', () => {
+  const props = {
+    activeView: views.grades,
+    areGradesFrozen: false,
+    canUserViewGradebook: false,
+    courseId,
+    showBulkManagement: false,
+  };
+  beforeEach(() => {
+    props.setView = jest.fn();
+  });
   describe('snapshots', () => {
+    let el;
+    beforeEach(() => {
+      el = shallow(<GradebookHeader {...props} />);
+      el.instance().handleToggleViewClick = jest.fn().mockName('this.handleToggleViewClick');
+    });
     describe('default values (grades frozen, cannot view).', () => {
       test('unauthorized warning, but no grades frozen warning', () => {
-        const props = { courseId, areGradesFrozen: false, canUserViewGradebook: false };
-        expect(shallow(<GradebookHeader {...props} />)).toMatchSnapshot();
+        expect(el.instance().render()).toMatchSnapshot();
       });
     });
     describe('grades frozen, cannot view', () => {
       test('unauthorized warning, and grades frozen warning.', () => {
-        const props = { courseId, areGradesFrozen: true, canUserViewGradebook: false };
-        expect(shallow(<GradebookHeader {...props} />)).toMatchSnapshot();
+        el.setProps({ areGradesFrozen: true });
+        expect(el.instance().render()).toMatchSnapshot();
       });
     });
     describe('grades frozen, can view.', () => {
       test('grades frozen warning but no unauthorized warning', () => {
-        const props = { courseId, areGradesFrozen: true, canUserViewGradebook: true };
-        expect(shallow(<GradebookHeader {...props} />)).toMatchSnapshot();
+        el.setProps({ areGradesFrozen: true, canUserViewGradebook: true });
+        expect(el.instance().render()).toMatchSnapshot();
+      });
+    });
+    describe('show bulk management, active view is grades view', () => {
+      test('toggle view button to activity log', () => {
+        el.setProps({ showBulkManagement: true });
+        expect(el.find(Button).getElement()).toEqual((
+          <Button
+            variant="tertiary"
+            onClick={el.instance().handleToggleViewClick}
+          >
+            <FormattedMessage {...messages.toActivityLog} />
+          </Button>
+        ));
+        expect(el.instance().render()).toMatchSnapshot();
+      });
+    });
+    describe('show bulk management, active view is activity view', () => {
+      test('toggle view button to grades', () => {
+        el.setProps({ showBulkManagement: true, activeView: views.activity });
+        expect(el.find(Button).getElement()).toEqual((
+          <Button
+            variant="tertiary"
+            onClick={el.instance().handleToggleViewClick}
+          >
+            <FormattedMessage {...messages.toGradesView} />
+          </Button>
+        ));
+        expect(el.instance().render()).toMatchSnapshot();
+      });
+    });
+  });
+  describe('behavior', () => {
+    let el;
+    beforeEach(() => {
+      el = shallow(<GradebookHeader {...props} />);
+    });
+    describe('handleToggleViewClick', () => {
+      test('calls setView with activity view if activeView is grades', () => {
+        el.instance().handleToggleViewClick();
+        expect(props.setView).toHaveBeenCalledWith(views.activity);
+      });
+      test('calls setView with grades view if activeView is activity', () => {
+        el.setProps({ activeView: views.activity });
+        el.instance().handleToggleViewClick();
+        expect(props.setView).toHaveBeenCalledWith(views.grades);
       });
     });
   });
@@ -45,6 +122,9 @@ describe('GradebookHeader component', () => {
     const testState = { a: 'test', example: 'state' };
     beforeEach(() => {
       mapped = mapStateToProps(testState);
+    });
+    test('activeView from app.activeView', () => {
+      expect(mapped.activeView).toEqual(selectors.app.activeView(testState));
     });
     test('courseId from app.courseId', () => {
       expect(mapped.courseId).toEqual(selectors.app.courseId(testState));
@@ -58,6 +138,15 @@ describe('GradebookHeader component', () => {
       expect(
         mapped.canUserViewGradebook,
       ).toEqual(selectors.roles.canUserViewGradebook(testState));
+    });
+    test('showBulkManagement from root showBulkManagement selector', () => {
+      expect(mapped.showBulkManagement).toEqual(selectors.root.showBulkManagement(testState));
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    test('setView from actions.app.setView', () => {
+      expect(mapDispatchToProps.setView).toEqual(actions.app.setView);
     });
   });
 });
