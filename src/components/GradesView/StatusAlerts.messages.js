a0e1a60d23685d1a86c8a75f1ae24e865725a955
Carlos Muniz <cmuniz@tcril.org>
2021-12-03 09:23:17 -0500

    docs: Describe messages descriptions better
    These messages have been updated to be more descriptive.
    The message descriptions need to be more descriptive in order to
    be easily translated (i18n) into other languages.

--- src/components/GradesView/StatusAlerts.messages.js
+++ src/components/GradesView/StatusAlerts.messages.js
@@ -4,17 +4,17 @@ const messages = defineMessages({
   editSuccessAlert: {
     id: 'gradebook.GradesView.editSuccessAlert',
     defaultMessage: 'The grade has been successfully edited.  You may see a slight delay before updates appear in the Gradebook.',
-    description: 'Alert text for successful edit action',
+    description: 'An alert text for successfully editing  a grade',
   },
   maxGradeInvalid: {
     id: 'gradebook.GradesView.maxCourseGradeInvalid',
     defaultMessage: 'Maximum course grade must be between 0 and 100',
-    description: 'Alert text for invalid maximum course grade',
+    description: 'An alert text for selecting a maximum course grade greater than 100',
   },
   minGradeInvalid: {
     id: 'gradebook.GradesView.minCourseGradeInvalid',
     defaultMessage: 'Minimum course grade must be between 0 and 100',
-    description: 'Alert text for invalid minimum course grade',
+    description: 'An alert text for selecting a minimum course grade less than 0',
   },
 });
 
--- src/data/constants/filters.messages.js
+++ src/data/constants/filters.messages.js
@@ -4,37 +4,37 @@ const messages = defineMessages({
   assignment: {
     id: 'gradebook.GradesTab.FilterBadges.assignment',
     defaultMessage: 'Assignment',
-    description: 'Assignment FilterBadge label',
+    description: 'A label describing the notification under the "Edit Filters" button that shows by which of the course\'s assignments the view is being filtered.',
   },
   assignmentGrade: {
     id: 'gradebook.GradesTab.FilterBadges.assignmentGrade',
     defaultMessage: 'Assignment Grade',
-    description: 'Assignment Grade FilterBadge label',
+    description: 'A label describing the notification under the "Edit Filters" button that shows that the view is being filtered to include assignment grades within the alloted range.',
   },
   assignmentType: {
     id: 'gradebook.GradesTab.FilterBadges.assignmentType',
     defaultMessage: 'Assignment Type',
-    description: 'Assignment Type FilterBadge label',
+    description: 'A label describing the notification under the "Edit Filters" button that shows by which of the course\'s assignment types the view is being filtered.',
   },
   cohort: {
     id: 'gradebook.GradesTab.FilterBadges.cohort',
     defaultMessage: 'Cohort',
-    description: 'Cohort FilterBadge label',
+    description: 'A label describing the notification under the "Edit Filters" button that shows by which of the course\'s cohorts the view is being filtered.',
   },
   courseGrade: {
     id: 'gradebook.GradesTab.FilterBadges.courseGrade',
     defaultMessage: 'Course Grade',
-    description: 'Course Grade FilterBadge label',
+    description: 'A label describing the notification under the "Edit Filters" button that shows that the view is being filtered to include course grades within the alloted range.',
   },
   includeCourseRoleMembers: {
     id: 'gradebook.GradesTab.FilterBadges.includeCourseRoleMembers',
     defaultMessage: 'Include Course Team Members',
-    description: 'Include Course Team Members FilterBadge label',
+    description: 'A label describing the notification under the "Edit Filters" button that shows that the view is being filtered to include course team members.',
   },
   track: {
     id: 'gradebook.GradesTab.FilterBadges.track',
     defaultMessage: 'Track',
-    description: 'Track FilterBadge label',
+    description: 'A label describing the notification under the "Edit Filters" button that shows by which of the course\'s tracks the view is being filtered.',
   },
 });
 
