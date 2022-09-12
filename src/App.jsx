c0167643f4f7fb8370a49ab1a263aeda6bcd5d37
Diana Catalina Olarte <diana.olarte@edunext.co>
2022-09-12 09:55:36 -0500

    test: update App test and fix Head text typo

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Head from './head/Head';
 
 const App = () => (
   <AppProvider store={store}>
+    <Head />
     <Router>
       <div>
         <Header />
@@ -29,7 +30,6 @@ const App = () => (
         <Footer logo={process.env.LOGO_POWERED_BY_OPEN_EDX_URL_SVG} />
       </div>
     </Router>
-    <Head />
   </AppProvider>
 );
 
