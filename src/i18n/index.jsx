89230ad998a048b344818338a96483cce1b2999b
Emad Rad <codewithemad@gmail.com>
2022-10-17 11:18:29 +0330

    feat: Persian language fa-ir added to messages

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -1,4 +1,7 @@
 import arMessages from './messages/ar.json';
+// no need to import en messages-- they are in the defaultMessage field
+import es419Messages from './messages/es_419.json';
+import faIRMessages from './messages/fa_IR.json';
 import frMessages from './messages/fr.json';
 import es419Messages from './messages/es_419.json';
 import zhcnMessages from './messages/zh_CN.json';
@@ -14,6 +17,7 @@ import frCAMessages from './messages/fr_CA.json';
 const messages = {
   ar: arMessages,
   'es-419': es419Messages,
+  'fa-ir': faIRMessages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
   pt: ptMessages,
