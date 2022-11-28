c3b16d026b726691caf86ea7b3791032312663a0
Kyle McCormick <kdmc@pm.me>
2022-11-28 10:24:32 -0500

    test: cover call to `initialize(...)` in index.jsx
    Add a heavily-mocked test to make Coverage happy.
    Based on a similar test from:
    https://github.com/edx/frontend-app-ora-grading/blob/open-release/olive.master/src/index.test.jsx#L76-L86
    
    I guess what we are testing here is that `initialize` and
    `mergeConfig` are being called with the correct arguments.
    I'm not convinced this test is valuable, but 100% coverage
    is required on this repo, so here it is.

--- src/index.test.jsx
+++ src/index.test.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import {
   APP_READY,
   initialize,
+  mergeConfig,
   subscribe,
 } from '@edx/frontend-platform';
 import { messages as headerMessages } from '@edx/frontend-component-header';
@@ -19,6 +20,7 @@ jest.mock('react-dom', () => ({
 jest.mock('@edx/frontend-platform', () => ({
   APP_READY: 'app-is-ready-key',
   initialize: jest.fn(),
+  mergeConfig: jest.fn(),
   subscribe: jest.fn(),
 }));
 jest.mock('@edx/frontend-component-header', () => ({
@@ -54,5 +56,15 @@ describe('app registry', () => {
         config: expect.any(Function),
       },
     });
+  });
+  test('initialize config loads LMS_BASE_URL from env', () => {
+    const oldEnv = process.env;
+    const initializeArg = initialize.mock.calls[0][0];
+    process.env = { ...oldEnv, LMS_BASE_URL: 'http://example.com/fake' };
+    initializeArg.handlers.config();
+    expect(mergeConfig).toHaveBeenCalledWith(
+      expect.objectContaining({ LMS_BASE_URL: 'http://example.com/fake' }),
+    );
+    process.env = oldEnv;
   });
 });
