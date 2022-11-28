4f43e65f039a8c8464b35326dcbd11c8a67f777b
Kyle McCormick <kdmc@pm.me>
2022-11-28 11:14:32 -0500

    fix: use getConfig in order to support runtime configuration (#286)
    Before, gradebook was reading config from `process.env`
    directly, which locked the app into using only
    static (build-time) configuration. In order to
    enable dynamic (runtime) configuration, we update
    gradebook to use frontend-platform's standard
    configuration interface: `mergeConfig()` and `getConfig()`.
    
    Bumps version from 1.5.0 to 1.6.0.
    (I would normally just do a patch release for a fix, but the version
     was hasn't been bumped for a while, so adding in full runtime
     configuration support seemed like it warranted a proper
     minor version bump.)
    
    Co-authored-by: Ghassan Maslamani <ghassan.maslamani@gmail.com>

--- src/data/store.js
+++ src/data/store.js
@@ -4,19 +4,19 @@ import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProductio
 import { createLogger } from 'redux-logger';
 import { createMiddleware } from 'redux-beacon';
 import Segment from '@redux-beacon/segment';
+import { getConfig } from '@edx/frontend-platform';
 
 import actions from './actions';
 import selectors from './selectors';
 import reducers from './reducers';
 import eventsMap from './services/segment/mapping';
-import { configuration } from '../config';
 
 export const createStore = () => {
   const loggerMiddleware = createLogger();
 
   const middleware = [thunkMiddleware, loggerMiddleware];
   // Conditionally add the segmentMiddleware only if the SEGMENT_KEY environment variable exists.
-  if (configuration.SEGMENT_KEY) {
+  if (getConfig().SEGMENT_KEY) {
     middleware.push(createMiddleware(eventsMap, Segment()));
   }
   const store = redux.createStore(
--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import ReactDOM from 'react-dom';
 import {
   APP_READY,
   initialize,
+  mergeConfig,
   subscribe,
 } from '@edx/frontend-platform';
 import { messages as headerMessages } from '@edx/frontend-component-header';
@@ -20,6 +21,22 @@ subscribe(APP_READY, () => {
 });
 
 initialize({
+  handlers: {
+    config: () => {
+      mergeConfig({
+        BASE_URL: process.env.BASE_URL,
+        LMS_BASE_URL: process.env.LMS_BASE_URL,
+        LOGIN_URL: process.env.LOGIN_URL,
+        LOGOUT_URL: process.env.LOGOUT_URL,
+        CSRF_TOKEN_API_PATH: process.env.CSRF_TOKEN_API_PATH,
+        REFRESH_ACCESS_TOKEN_ENDPOINT: process.env.REFRESH_ACCESS_TOKEN_ENDPOINT,
+        DATA_API_BASE_URL: process.env.DATA_API_BASE_URL,
+        SECURE_COOKIES: process.env.NODE_ENV !== 'development',
+        SEGMENT_KEY: process.env.SEGMENT_KEY,
+        ACCESS_TOKEN_COOKIE_NAME: process.env.ACCESS_TOKEN_COOKIE_NAME,
+      });
+    },
+  },
   messages: [
     appMessages,
     headerMessages,
