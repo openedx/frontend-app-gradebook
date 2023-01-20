6a21c1112abe514e3a636f30149f3d71df01609a
Emad <codewithemad@gmail.com>
2023-01-20 19:56:05 +0330

    refactor: reorder imports alphabetically

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -1,17 +1,15 @@
 import arMessages from './messages/ar.json';
-// no need to import en messages-- they are in the defaultMessage field
+import deMessages from './messages/de.json';
 import es419Messages from './messages/es_419.json';
 import faIRMessages from './messages/fa_IR.json';
 import frMessages from './messages/fr.json';
-import es419Messages from './messages/es_419.json';
-import zhcnMessages from './messages/zh_CN.json';
-import ptMessages from './messages/pt.json';
-import itMessages from './messages/it.json';
-import ukMessages from './messages/uk.json';
-import deMessages from './messages/de.json';
-import ruMessages from './messages/ru.json';
-import hiMessages from './messages/hi.json';
 import frCAMessages from './messages/fr_CA.json';
+import hiMessages from './messages/hi.json';
+import itMessages from './messages/it.json';
+import ptMessages from './messages/pt.json';
+import ruMessages from './messages/ru.json';
+import ukMessages from './messages/uk.json';
+import zhcnMessages from './messages/zh_CN.json';
 // no need to import en messages-- they are in the defaultMessage field
 
 const messages = {
