f41b237d08b60537095e4421320e2bc53d1dd7f8
Ben Warzeski <bwarzeski@edx.org>
2023-05-11 13:41:33 -0400

    fix: redux transform tests

--- src/data/redux/transforms.js
+++ src/data/redux/transforms.js
@@ -1,11 +1,6 @@
 import { StrictDict } from 'utils';
 import selectors from 'data/selectors';
 
-// export const root = StrictDict({});
-// export const app = StrictDict({});
-export const assignmentTypes = StrictDict({});
-export const cohorts = StrictDict({});
-export const filters = StrictDict({});
 export const grades = StrictDict({
   subsectionGrade: ({ gradeFormat, subsection }) => () => (
     selectors.grades.subsectionGrade[gradeFormat](subsection)
@@ -13,19 +8,6 @@ export const grades = StrictDict({
   roundGrade: selectors.grades.roundGrade,
 });
 
-export const roles = StrictDict({
-});
-
-export const tracks = StrictDict({
-});
-
 export default StrictDict({
-  app,
-  assignmentTypes,
-  cohorts,
-  filters,
   grades,
-  roles,
-  tracks,
-  root,
 });
