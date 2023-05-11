4cf5ba7a07abdc25f00aca3385c1dcca1554d4bd
Ben Warzeski <bwarzeski@edx.org>
2023-05-11 13:28:51 -0400

    refactor: FilteredUsersLabel component modernization

---
+++ src/components/GradesView/FilteredUsersLabel/messages.js
@@ -0,0 +1,11 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  visibilityLabel: {
+    id: 'gradebook.GradesTab.usersVisibilityLabel',
+    defaultMessage: 'Showing {filteredUsers} of {totalUsers} total learners',
+    description: 'Users visibility label',
+  },
+});
+
+export default messages;
