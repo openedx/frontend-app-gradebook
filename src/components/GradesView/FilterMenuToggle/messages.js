7a506324a81343f512a0295388c5a2b1dd36c479
Ben Warzeski <bwarzeski@edx.org>
2023-05-11 13:28:38 -0400

    refactor: FilterMenuToggle component modernization

---
+++ src/components/GradesView/FilterMenuToggle/messages.js
@@ -0,0 +1,11 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  editFilters: {
+    id: 'gradebook.GradesView.editFilterLabel',
+    defaultMessage: 'Edit Filters',
+    description: 'A labeled button in the Grades tab that opens/closes the Filters tab, allowing the grades to be filtered',
+  },
+});
+
+export default messages;
