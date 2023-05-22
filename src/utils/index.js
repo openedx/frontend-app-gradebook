64cfcd92a26d3fad70f8c3b435a6e5a12e55ef1a
Ben Warzeski <bwarzesk@gmail.com>
2023-05-22 19:50:29 +0000

    chore: top-level formatDate util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as StrictDict } from './StrictDict';
 export { default as keyStore } from './keyStore';
+export { default as formatDateForDisplay } from './formatDate';
