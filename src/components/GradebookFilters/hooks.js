f1297228112d0f883b0099ec276d4acd168429ef
Ben Warzeski <bwarzesk@gmail.com>
2023-05-22 19:55:03 +0000

    refactor: update GradebookFilters components

--- src/components/GradebookFilters/hooks.js
+++ src/components/GradebookFilters/hooks.js
@@ -3,7 +3,7 @@ import { actions, selectors, thunkActions } from 'data/redux/hooks';
 export const useGradebookFiltersData = ({ updateQueryParams }) => {
   const includeCourseRoleMembers = selectors.filters.useIncludeCourseRoleMembers();
   const updateIncludeCourseRoleMembers = actions.filters.useUpdateIncludeCourseRoleMembers();
-  const closeMenu = thunkActions.app.useCloseFilterMenu();
+  const closeMenu = thunkActions.app.filterMenu.useCloseMenu();
   const fetchGrades = thunkActions.grades.useFetchGrades();
 
   const handleIncludeTeamMembersChange = ({ target: { checked } }) => {
