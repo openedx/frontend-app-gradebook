869445280c03d00eb1a206962e328cc52d231148
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-09-03 16:27:26 -0600

    test: improve coverage on gradebook table

--- src/components/GradesView/GradebookTable/hooks.test.jsx
+++ src/components/GradesView/GradebookTable/hooks.test.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Headings } from 'data/constants/grades';
 
 import { initializeMocks, render } from '../../../testUtilsExtra';
 import * as hooks from './hooks';
+import messages from './messages';
 
 jest.unmock('@openedx/paragon');
 jest.unmock('react');
 jest.unmock('@edx/frontend-platform/i18n');
 
+let mockUseAllGrades;
+let mockUseGetHeadings;
+
 jest.mock('data/redux/hooks', () => ({
   selectors: {
-    grades: { useAllGrades: jest.fn(() => []) },
-    root: { useGetHeadings: jest.fn(() => []) },
+    grades: { useAllGrades: () => mockUseAllGrades() },
+    root: { useGetHeadings: () => mockUseGetHeadings() },
   },
 }));
 
@@ -27,7 +32,69 @@ jest.mock('./LabelReplacements', () => ({
   MastersOnlyLabelReplacement: () => null,
 }));
 
+const subsectionLabels = [
+  'subsectionLabel1',
+  'subsectionLabel2',
+  'subsectionLabel3',
+];
+
+const allGrades = [
+  {
+    username: 'test-username-1',
+    external_user_key: 'EKey1',
+    email: 'email-1',
+    fullName: 'test-fullNAME',
+    percent: 0.9,
+    section_breakdown: [
+      { label: subsectionLabels[0] },
+      { label: subsectionLabels[1] },
+      { label: subsectionLabels[2] },
+    ],
+  },
+  {
+    username: 'test-username-2',
+    external_user_key: 'EKey2',
+    email: 'email-2',
+    percent: 0.8,
+    section_breakdown: [
+      { label: subsectionLabels[0] },
+      { label: subsectionLabels[1] },
+      { label: subsectionLabels[2] },
+    ],
+  },
+  {
+    username: 'test-username-3',
+    external_user_key: 'EKey3',
+    email: 'email-3',
+    percent: 0.6,
+    section_breakdown: [
+      { label: subsectionLabels[0] },
+      { label: subsectionLabels[1] },
+      { label: subsectionLabels[2] },
+    ],
+  },
+];
+
+const testHeading = 'test-heading-value';
+
+const headings = [
+  Headings.totalGrade,
+  Headings.username,
+  Headings.email,
+  Headings.fullName,
+  testHeading,
+];
+
 describe('useGradebookTableData hook', () => {
+  beforeAll(() => {
+    mockUseAllGrades = jest.fn();
+    mockUseGetHeadings = jest.fn();
+  });
+
+  beforeEach(() => {
+    mockUseAllGrades.mockReset();
+    mockUseGetHeadings.mockReset();
+  });
   let hookResult;
 
   const TestComponent = () => {
@@ -38,6 +105,8 @@ describe('useGradebookTableData hook', () => {
   beforeEach(() => {
     initializeMocks();
     hookResult = null;
+    mockUseAllGrades.mockReturnValue([]);
+    mockUseGetHeadings.mockReturnValue([]);
   });
 
   it('returns expected structure with empty data', () => {
@@ -54,5 +123,21 @@ describe('useGradebookTableData hook', () => {
   it('nullMethod returns null', () => {
     render(<TestComponent />);
     expect(hookResult.nullMethod()).toBeNull();
+  });
+
+  it('returns expected structure with grades and headings data', () => {
+    mockUseAllGrades.mockReturnValue(allGrades);
+    mockUseGetHeadings.mockReturnValue(headings);
+    render(<TestComponent />);
+    expect(hookResult.columns.length).toBe(headings.length);
+    expect(hookResult.columns[0].accessor).toEqual(headings[0]);
+    expect(hookResult.data.length).toBe(allGrades.length);
+    expect(hookResult.data[0]).toHaveProperty(Headings.username);
+    expect(hookResult.grades).toEqual(allGrades);
+    expect(hookResult.nullMethod()).toBeNull();
+    expect(hookResult.emptyContent).toBe(messages.noResultsFound.defaultMessage);
+
+    expect(mockUseAllGrades).toHaveBeenCalled();
+    expect(mockUseGetHeadings).toHaveBeenCalled();
   });
 });
